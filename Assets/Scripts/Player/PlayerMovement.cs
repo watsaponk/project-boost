e3855ec1c6dfc8440e06047e56ce7c493efa6ab2
Watsapon Kampong <k.watsapon@gmail.com>
2021-09-04 09:49:54 +0700

    refactor: update movement by fore

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,15 +4,34 @@ namespace Player
 {
     public class PlayerMovement : MonoBehaviour
     {
-        [SerializeField] private float moveSpeed = 10f;
+        [SerializeField] private float thrust = 1f;
+        [SerializeField] private float rotationSpeed = 1;
+
+        private Rigidbody _rigidbody;
+
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
 
         private void Update()
         {
-            float speed = moveSpeed * Time.deltaTime;
-            float xMove = speed * Input.GetAxis("Horizontal");
-            float yMove = speed * Input.GetAxis("Vertical");
+            ProcessThrust();
+            ProcessRotation();
+        }
 
-            transform.Translate(xMove, yMove, 0);
+        private void ProcessThrust()
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                _rigidbody.AddRelativeForce(Vector3.up * thrust * Time.deltaTime);
+            }
+        }
+
+        private void ProcessRotation()
+        {
+            float rotate = rotationSpeed * Time.deltaTime * Input.GetAxis("Horizontal") * -1;
+            transform.Rotate(0, 0, rotate);
         }
     }
 }
