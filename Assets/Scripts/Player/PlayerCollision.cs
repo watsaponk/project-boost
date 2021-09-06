4d0c2b7d5c658a50198ff231ec72f50a7a922ce1
Watsapon Kampong <k.watsapon@gmail.com>
2021-09-06 08:38:49 +0700

    feat: Reload scene when hit the ground

---
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Player
+{
+    public class PlayerCollision : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision other)
+        {
+            switch (other.gameObject.tag)
+            {
+                case "Finish":
+                {
+                    Debug.Log("Finish");
+                    break;
+                }
+                case "Friendly":
+                {
+                    Debug.Log("Friendly");
+                    break;
+                }
+                case "Fuel":
+                {
+                    Debug.Log("Fuel");
+                    break;
+                }
+                default:
+                {
+                    Debug.Log("Destroy");
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                    break;
+                }
+            }
+        }
+    }
+}
